2605feed1833799da74dfbe0c1024288e514aa54
FiocChuck <chasf@google.com>
2023-04-17 18:45:40 -0400

    change start date

--- dags/trigger-dataflow.py
+++ dags/trigger-dataflow.py
@@ -17,7 +17,7 @@ with DAG(
     dag_id='trigger-dataflow',
     default_args=args,
     schedule_interval='@once',  # set schedule - at every tenth minute
-    start_date=days_ago(1),
+    start_date=days_ago(0),
     is_paused_upon_creation=True
 
 ) as dag:
